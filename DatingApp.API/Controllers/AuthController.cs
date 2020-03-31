3ff30c68c754a7d7138890708e95af65e0bffe5e
vikash ranjan <vikash5r@hotmail.com>
2020-03-31 18:36:45 +0530

    created login and register method

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register (string username, string password)
+        {
+            username = username.ToLower();
+
+            if(await _repo.UserExists(username))
+            {
+                return BadRequest("User already exists.");
+            }
+            var userToCreate = new User
+            {
+                UserName = username
+            };
+            var createdUser = await _repo.Register(userToCreate, password);
+
+            return StatusCode(201);
+        }
+    }
+}
